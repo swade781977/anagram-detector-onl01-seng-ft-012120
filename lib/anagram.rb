c6543e8168d860b10d9c844be1f2d4697371b8d3
Chris Wade <swade781977@yahoo.com>
2020-02-04 01:36:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,14 +8,4 @@ class Anagram
   attr_accessor :word, :list
   
   def match(list)
-    matches =[]
-    list.each do |word|
-      l_a = word.split('').sort
-          if a.all?{|letters| l_a.include?(letters)}
-        matches.push(word)
-        else
-          puts "There are no Matches!"
-        end
-    end
-  end
-end
+   list
