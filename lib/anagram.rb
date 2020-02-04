45897808352cc0f5eec3a138c002c273af8058e0
Chris Wade <swade781977@yahoo.com>
2020-02-04 00:58:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,19 @@
 class Anagram
   def initialize (word)
     @word = word
+    @
     @list = []
   end
   attr_accessor :word :list
   
   def word_to_array(word)
-    w2a = @word.split
-    w2a = 
+    w2a = @word.split.sort
+    w2a
   end
   
   def match(@list)
     @list.each |word|
-      word.split
+      word.split.sort
       if @wor
   
 end
