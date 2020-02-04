52e456723fba0009708f5b56b8a6bb5fd17b73b9
Chris Wade <swade781977@yahoo.com>
2020-02-04 00:57:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  def initialize (word)
+    @word = word
+    @list = []
+  end
+  attr_accessor :word :list
+  
+  def word_to_array(word)
+    w2a = @word.split
+    w2a = 
+  end
+  
+  def match(@list)
+    @list.each |word|
+      word.split
+      if @wor
+  
+end
